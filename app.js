8e7f08ce9f9e3d5271b050a132a9c57a772d1a3f
Valentina Calderón <valecalderonc27@gmail.com>
2026-04-23 22:18:18 +0000

    feat: actualizar mensaje a Hola DevOps

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hola Arquitectos de Software — CI/CD funcionando al 100%!\n');
+  res.end('Hola DevOps!\n');
 });
 
 // Exportamos la función para que el test pueda usarla
